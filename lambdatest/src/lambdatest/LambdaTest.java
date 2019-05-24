c3205979ea86c527d2041c05d9aa28d3bbe393a4
fxzzq <1040175504@qq.com>
2019-05-24 09:26:15 +0800

    first commit

---
+++ lambdatest/src/lambdatest/LambdaTest.java
@@ -0,0 +1,32 @@
+package lambdatest;
+
+public class LambdaTest {
+	public static void main(String[] args) {
+		MathOperation add = (int x ,int y)-> x+y;
+		String s1="";
+		System.out.println(add.operation(1, 2));
+		
+	//	MathOperation sx= (String x) -> x+s1;
+		//sx.s("123");
+		
+		int x=4;
+	//	(x) -> 2 * x;
+		
+		
+		/*
+		 * 
+		 * 
+		 * 
+		 * System.out.println("10 + 5 ="+operator(10,5,add));
+		 */
+	new Thread(()->System.out.println("Hello World!010")).start();
+	
+	
+	}
+	
+	/*
+	 * private static int operator(int a,int b,MathOperation operation1){ return
+	 * operation1.operation(a,b); }
+	 */
+	//UnaryOperator
+}
---
+++ lambdatest/src/lambdatest/MathOperation.java
@@ -0,0 +1,9 @@
+package lambdatest;
+
+public interface MathOperation {
+	
+	int operation( int a, int b);
+	
+	//String  s(String s11);
+
+}
